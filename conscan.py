e8c9630713a80bc2fcd2002504a3a20ae34b83dc
James Campbell <jcampbell@primer.ai>
2019-11-13 10:59:20 -0500

    added list comprehension and removed extra else statements

--- conscan.py
+++ conscan.py
@@ -1,6 +1,5 @@
 """Uses brand new features of Python 3"""
-
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures import ThreadPoolExecutor
 import os
 import sys
 import time
@@ -9,7 +8,12 @@ start_time = time.time()
 
 
 def checker(ip_address):
-    # Your actual program here. Using threading.Lock() if necessary
+    """
+    checks if mac address found from nmap that matches raspberry pi
+    Accepts: ip_address var as string
+    Returns: nothing
+    Prints: found ip of pi if found
+    """
     data = check_output(f"nmap -sP {ip_address}", shell=True)
     if "B8:27:EB" in str(data):
         print(f'Found pi: {ip_address}')
@@ -27,42 +31,29 @@ logo = """
 
 """
 
+
 def main():
     # create as many threads as you want and set from terminal as parameter
     thread_count = 48
+    limit = 1
     if not os.geteuid() == 0:
         sys.exit('This script must be run as root (or with \'sudo\')!')
     if len(sys.argv) > 1:
         thread_count = int(sys.argv[1])
     currentnum = 1
     macme = input('What net to check? (default 10.2.2.0/24): ') or '10.2.2.0/24'
-    print(f'\nChecking each ip in {macme}...')
+    print(f'\nChecking for delicious pi around {macme}...')
     if macme.endswith('/24'):
         limit = 255
-    else:
-        limit = 1
     if limit == 1:
         checkip = macme.rsplit('.', 1)[0] + f'.{currentnum}'
-        data = check_output(f"nmap -sP {checkip}", shell=True)
-        if "B8:27:EB" in str(data):
-            print(f'Found pi: {macme}')
-        else:
-            pass
+        checker(checkip)
         print("--- %s seconds ---" % (time.time() - start_time))
-        exit()
-    else:
-        ip_list = []
-        while currentnum <= limit:
-            checkip = macme.rsplit('.', 1)[0] + f'.{currentnum}'
-            ip_list.append(checkip)
-            currentnum = currentnum + 1
+        sys.exit(0)
+    ip_list = []
+    ip_list.extend([macme.rsplit('.', 1)[0] + f'.{i}' for i in range(limit)])
     with ThreadPoolExecutor(max_workers=thread_count) as executor:
         {executor.submit(checker, ip) for ip in ip_list}
-
-        # as_completed() gives you the threads once finished
-        # for f in as_completed(futures):
-        # Get the results
-        # rs = f.result()
     print("--- %s seconds ---" % (time.time() - start_time))
 
 if __name__ == "__main__":
