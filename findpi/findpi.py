0c259a272b19c6cbe027a2ec50fd1ef9008f35e6
James Campbell <james@jamescampbell.us>
2020-02-12 15:20:17 -0500

    added core count sensing takes care of issue 9

--- findpi/findpi.py
+++ findpi/findpi.py
@@ -10,7 +10,19 @@ from subprocess import check_output
 __version__ = "1.0.4"
 
 
-def checker(ip_address):
+def checkCores():
+    """
+    Gets the total number of cores and returns sensible default for threads
+    """
+    multiplier = 4  # set this to whatever you want
+    operatingsystem = check_output("uname", shell=True)
+    if "Darwin" in str(operatingsystem):
+        cores = int(check_output("sysctl -n hw.ncpu", shell=True)) * multiplier
+    else:
+        cores = int(check_output("egrep 'cpu cores' /proc/cpuinfo | uniq | egrep -o '[0-9]'")) * multiplier
+    return cores
+
+def checkMacs(ip_address):
     """
     checks if mac address found from nmap that matches raspberry pi
     Accepts: ip_address var as string
@@ -40,8 +52,8 @@ def main():
     # get current ip hopefully and convert to /24
     currentip = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
     currentip = currentip.rsplit('.', 1)[0] + '.0/24'
-    # create as many threads as you want and set from terminal as parameter
-    thread_count = 48
+    # set sensible default cores count or override from argv below
+    thread_count = checkCores()
     limit = 1
     if not os.geteuid() == 0:
         sys.exit('This script must be run as root (or with \'sudo\')!')
@@ -56,7 +68,7 @@ def main():
         limit = 255
     if limit == 1:
         checkip = userinput.rsplit('.', 1)[0] + f'.{currentnum}'
-        checker(checkip)
+        checkMacs(checkip)
         print("--- %s seconds ---" % (time.time() - start_time))
         sys.exit(0)
     ip_list = []
@@ -65,7 +77,7 @@ def main():
                     f'.{i}' for i in range(limit)])
     # multi-threading the modern way ;)
     with ThreadPoolExecutor(max_workers=thread_count) as executor:
-        {executor.submit(checker, ip) for ip in ip_list}
+        {executor.submit(checkMacs, ip) for ip in ip_list}
     # always print the time it took to complete
     print("--- %s seconds ---" % (time.time() - start_time))
 
